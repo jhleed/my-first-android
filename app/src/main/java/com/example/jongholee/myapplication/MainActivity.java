67a3152e795f69729f3bf2b526acc5db994f55b1
jongholee <jhleed3@gmail.com>
2018-06-13 15:25:03 +0900

    이벤트 핸들러 추가 : 버튼 클릭시 입력한 텍스트의 값 뷰에 뿌려줌

--- app/src/main/java/com/example/jongholee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jongholee/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.jongholee.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button click_btn;
+    TextView displays_textView;
+    EditText input_editText;
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displays_textView.setText(input_editText.getText().toString());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click_btn = findViewById(R.id.click_btn);
+        displays_textView = findViewById(R.id.display_textview);
+        input_editText = findViewById(R.id.input_editText);
+
+        click_btn.setOnClickListener(mClickListener);
     }
 }
