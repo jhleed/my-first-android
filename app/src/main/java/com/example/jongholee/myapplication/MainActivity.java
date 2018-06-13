1945e03e4924d6269713e62e17a3f8cd836e268a
jongholee <jhleed3@gmail.com>
2018-06-13 15:59:38 +0900

    이벤트 핸들러 추가 : 텍스트 뷰 : 텍스트 뷰 클릭시 특정 처리
    - 분기를 이렇게 태우는구나..

--- app/src/main/java/com/example/jongholee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jongholee/myapplication/MainActivity.java
@@ -14,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener mClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            displays_textView.setText(input_editText.getText().toString());
+
+            switch (v.getId()) {
+                case R.id.display_textview:
+                    input_editText.setText(displays_textView.getText().toString());
+                    break;
+
+                case R.id.click_btn:
+                    displays_textView.setText(input_editText.getText().toString());
+                    break;
+            }
         }
     };
 
@@ -28,5 +37,6 @@ public class MainActivity extends AppCompatActivity {
         input_editText = findViewById(R.id.input_editText);
 
         click_btn.setOnClickListener(mClickListener);
+        displays_textView.setOnClickListener(mClickListener);
     }
 }
